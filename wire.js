97bc2016f75ac4c2c3304577f72dd1bf1a184ae8
Eric Arnebäck <arnebackeric@gmail.com>
2016-07-28 16:47:21 +0200

    Add sun and clean up lots.

--- wire.js
+++ wire.js
@@ -1,9 +1,3 @@
-/*
-
-  This examples demonstrates how we can render a height map, how to place out several models(using the batching feature), and how to
-  implement a simple fullscreen post-process effect(using the framebuffer feature) in REGL. The post-process effect is a simple box filter blur.
-*/
-
 const canvas = document.body.appendChild(document.createElement('canvas'))
 const fit = require('canvas-fit')
 const regl = require('../regl')(canvas)
@@ -19,26 +13,122 @@ camera.zoom(2000.0)
 
 const Z_FAR = 10000
 const Z_NEAR = 0.01
-
-
-// make texture.
-var texData = []
+const FOV = Math.PI / 4
 
 var x
 var z
 var y
-// line width
-var lw = 10
-for (y = 0; y < 256; y++) {
-  var r = []
-  for (x = 0; x < 256; x++) {
-    if (y < lw || y > (256 - lw) || x < lw || x > (256 - lw)) {
-      r.push([255, 255, 255, 255])
-    } else {
-      r.push([0, 0, 0, 255])
+var r
+
+function makeWireframeTexture () {
+  var texData = []
+
+  var lw = 10 // line width
+  for (y = 0; y < 256; y++) {
+    r = []
+    for (x = 0; x < 256; x++) {
+      if (y < lw || y > (256 - lw) || x < lw || x > (256 - lw)) {
+        r.push([255, 255, 255, 255])
+      } else {
+        r.push([0, 0, 0, 255])
+      }
     }
+    texData.push(r)
   }
-  texData.push(r)
+
+  // do gaussian blur on the image:
+
+  var tempTexData = []
+
+  for (y = 0; y < 256; y++) {
+    r = []
+    for (x = 0; x < 256; x++) {
+      var c = [0, 0, 0, 0]
+
+      for (var ax = -3; ax <= +3; ax++) {
+        for (var ay = -3; ay <= +3; ay++) {
+          var wy = y + ay
+          var wx = x + ax
+
+          if (wy < 0 || wx < 0 || wy > 255 || wx > 255) {
+            continue
+          }
+
+          var d = texData[wy][wx]
+
+          c = [
+            c[0] + d[0],
+            c[1] + d[1],
+            c[2] + d[2],
+            c[3] + d[3]
+          ]
+        }
+      }
+      var u = 49.0
+      r.push([c[0] / u, c[1] / u, c[2] / u, c[3] / u])
+    }
+    tempTexData.push(r)
+  }
+  texData = tempTexData
+
+  return texData
+}
+
+// lerp between two colors
+function lerp (c0, c1, x) {
+  return [
+    c1[0] * x + c0[0] * (1.0 - x),
+    c1[1] * x + c0[1] * (1.0 - x),
+    c1[2] * x + c0[2] * (1.0 - x),
+    c1[3] * x + c0[3] * (1.0 - x)
+  ]
+}
+
+function makeSunTexture () {
+  // make texture.
+  var texData = []
+
+  // #F6 7D CA = 246, 125, 202
+  // #F73C6F =   247, 27, 111
+  var palette = [
+    [0.0, [246.0, 125.0, 202.0, 255.0]],
+    [0.6, [247.0, 27.0, 111.0, 255.0]],
+    [0.9, [247.0, 27.0, 111.0, 255.0]],
+    [1.0, [0.0, 0.0, 0.0, 255.0]]
+  ]
+//  var ip = 0 // current palette index.
+
+  for (y = 0; y < 256; y++) {
+    r = []
+    for (x = 0; x < 256; x++) {
+      // convert (x,y) to range [-1, +1]
+      var ox = (x - 128) / 127
+      var oy = (y - 128) / 127
+
+      var R = Math.sqrt(ox * ox + oy * oy)
+      var c
+
+      if (R >= 1.0) {
+        c = [0.0, 0.0, 0.0, 0.0]
+      } else {
+        var ip
+        for (ip = 0; ip < palette.length - 1; ip++) {
+          if (palette[ip][0] <= R && palette[ip + 1][0] >= R) {
+            break
+          }
+        }
+
+        var c0 = palette[ip + 0]
+        var c1 = palette[ip + 1]
+
+        c = lerp(c0[1], c1[1], (R - c0[0]) / (c1[0] - c0[0]))
+      }
+      r.push(c)
+    }
+    texData.push(r)
+  }
+
+  return texData
 }
 
 // geometry arrays.
@@ -58,6 +148,28 @@ var zmax = +(H / 2.0) * size
 var row
 var col
 
+function Chunk () {
+  this.position = []
+  this.positionBuffer = regl.buffer({
+    length: (H + 1) * (W + 1) * 3 * 4,
+    type: 'float',
+    usage: 'dynamic'
+  })
+}
+
+var chunkPool = []
+function allocChunk () {
+  return chunkPool.pop() || new Chunk()
+}
+function freeChunk (chunk) {
+  chunkPool.push(chunk)
+}
+
+function makeChunk () {
+
+}
+
+var j = 0
 for (row = 0; row <= H; ++row) {
   z = (row / H) * (zmax - zmin) + zmin
   for (col = 0; col <= W; ++col) {
@@ -75,48 +187,25 @@ for (row = 0; row <= H; ++row) {
       f *= 0.8
     }
 
-    n = Math.round(n / 60) * 60
-//    console.log('n: ', n)
-
-    position.push([x, n, z])
+    y = Math.round(n / 60) * 60
+    position[j++] = [x, y, z]
   }
 }
 
+const positionBuffer = regl.buffer({
+  length: (H + 1) * (W + 1) * 3 * 4,
+  type: 'float',
+  usage: 'dynamic'
+})
+
 for (row = 0; row <= H; ++row) {
-  z = (row / H)
-
+  z = (row)
   for (col = 0; col <= W; ++col) {
-    x = (col / W)
-
+    x = (col)
     texCoord.push([x, z])
   }
 }
 
-/*
-var i0
-var i1
-for (row = 0; row <= (H - 1); ++row) {
-  for (col = 0; col < (W - 1); ++col) {
-    i = row * (W + 1) + col
-
-    i0 = i + 0
-    i1 = i + 1
-
-    elements.push([i0, i1])
-  }
-}
-
-for (row = 0; row < (H - 1); ++row) {
-  for (col = 0; col <= (W - 1); ++col) {
-    i = row * (W + 1) + col
-
-    i0 = i + 0
-    i1 = i + (W + 1)
-
-    elements.push([i0, i1])
-  }
-}
-*/
 for (row = 0; row <= (H - 1); ++row) {
   for (col = 0; col <= (W - 1); ++col) {
     i = row * (W + 1) + col
@@ -131,31 +220,32 @@ for (row = 0; row <= (H - 1); ++row) {
   }
 }
 
-const drawTerrain = regl({
-
+const globalScope = regl({
+  uniforms: {
+    projection: ({viewportWidth, viewportHeight}) => {
+      return mat4.perspective([], FOV, viewportWidth / viewportHeight, Z_NEAR, Z_FAR)
+    }
+  },
   cull: {
     enable: true
-  },
+  }
+})
 
+const chunkScope = regl({
   uniforms: {
     // View Projection matrices.
     view: (_, props) => props.view,
-    projection: ({viewportWidth, viewportHeight}) =>
-      mat4.perspective([],
-                       Math.PI / 4,
-                       viewportWidth / viewportHeight,
-                       Z_NEAR,
-                       Z_FAR),
 
     tex: regl.texture({
       min: 'nearest mipmap linear',
       mag: 'linear',
       wrap: 'repeat',
-      data: texData
+      data: makeWireframeTexture()
     }),
     cameraPos: (_, props) => {
       return cameraPosFromViewMatrix([], props.view)
-    }
+    },
+    tick: ({tick}) => tick
   },
 
   frag: `
@@ -166,22 +256,23 @@ const drawTerrain = regl({
 
   uniform sampler2D tex;
 
-#define H int(${H})
-#define W int(${W})
-
   uniform vec3 cameraPos;
+  uniform float tick;
 
   void main () {
-    float dist = distance(cameraPos.xz, vPosition.xz);
+    vec3 p = vPosition;
+    vec3 d = vec3(1.0, 0.2, 0.5);
 
-    vec3 d = mix(vec3(1.0, 0.2, 0.5), vec3(1.0, 1.0, 1.0), dist / 1000.0);
-    d = vec3(1.0, 0.2, 0.5);
+    d = vec3(
+      (sin(tick*0.02 + 0.0) + 1.0) * 0.5 + 0.5,
+      (sin(tick*0.02 + 2.0) + 1.0) * 0.5 + 0.5,
+      (sin(tick*0.01 + 4.0) + 1.0) * 0.5 + 0.5
+    );
 
-    vec3 c = texture2D(tex, vTexCoord * vec2(W, H)).x * d;
+    vec3 c = texture2D(tex, vTexCoord).x * d;
     gl_FragColor = vec4(c.xyz, 1.0);
   }`,
   vert: `
-  uniform sampler2D heightTexture;
 
   precision mediump float;
 
@@ -207,29 +298,114 @@ const drawTerrain = regl({
   }`,
 
   attributes: {
-    position: regl.prop('position'),
-    texCoord: regl.prop('texCoord')
-
+    texCoord: texCoord
   },
-  elements: regl.prop('elements'),
-  primitive: 'triangles'
+  elements: elements
+})
+
+const drawSun = regl({
+  uniforms: {
+    view: (_, props) => {
+      var m = mat4.copy([], props.view)
+      m[12] = 0
+      m[13] = 0
+      m[14] = 0
+      return m
+    },
+    tex: regl.texture({
+      data: makeSunTexture(),
+      mag: 'linear'
+    })
+  },
+
+  frag: `
+  precision mediump float;
+
+  varying vec2 vTexCoord;
+
+  uniform sampler2D tex;
+
+  void main () {
+    gl_FragColor = vec4(texture2D(tex, vTexCoord).xyz, 1.0);
+  }`,
+  vert: `
+  precision mediump float;
+
+  attribute vec3 position;
+  attribute vec2 texCoord;
+
+  uniform mat4 projection, view;
+  uniform vec3 cameraPos;
+
+  varying vec2 vTexCoord;
+
+  void main() {
+    vec3 q = position;
+    q += vec3(0.0, -0.7, 0.0);
+    q *= vec3(vec2(0.4), -1.0);
+    vec4 p = view * vec4(q, 1);
+
+    vTexCoord = texCoord;
+    gl_Position = projection * p;
+  }`,
+
+  attributes: {
+    position: [
+      [-0.5, -0.5, 1.0],
+      [+0.5, -0.5, 1.0],
+      [+0.5, +0.5, 1.0],
+
+      [+0.5, +0.5, 1.0],
+      [-0.5, +0.5, 1.0],
+      [-0.5, -0.5, 1.0]
+    ],
+    texCoord: [
+      [0.0, 0.0],
+      [1.0, 0.0],
+      [1.0, 1.0],
+
+      [1.0, 1.0],
+      [0.0, 1.0],
+      [0.0, 0.0]
+    ]
+  },
+
+  count: 6,
+  depth: {
+    enable: false // the sun will be behind everything else.
+  }
+})
+
+var usePositionBuffer = positionBuffer
+const drawChunk = regl({
+  attributes: {
+    position: {
+      buffer: positionBuffer,
+      normalized: true
+    }
+  }
 })
 
 regl.frame(({deltaTime, viewportWidth, viewportHeight, tick}) => {
   regl.clear({color: [0.0, 0.0, 0.0, 1.0]})
 
   var view = camera.view()
-  //  tick = 0
-  var speed = 8.0
+  var speed = 19.0
   var startZ = 5100
   var down = -4000
-
+//  tick = 0.0
   mat4.lookAt(view, [0, 1000, startZ - tick * speed], [0, down, -startZ - tick * speed], [0, 1, 0])
-  drawTerrain({elements, position, texCoord,
-               view: view
 
-              })
+  positionBuffer.subdata(position)
+
+  globalScope(() => {
+    drawSun({view: view})
+
+    chunkScope({view: view}, () => {
+      // usePositionBuffer = positionBuffer
+      drawChunk({positionBuffer: positionBuffer})
+    })
+  })
 
   camera.tick()
 })
-//    gl_Position = planetCurvedVertex(positionIn.xyz, 0.003, mvp, posRelativeToCamera);
